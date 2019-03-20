9a52a780825daa80bc9e8c2cee5fdaecc8089efc
Caupo Helvik <caupohelvik@users.noreply.github.com>
2019-03-20 15:43:32 +0200

    UI pooleli kontroll kas won

--- TripsuTestid1.java
+++ TripsuTestid1.java
@@ -2,14 +2,15 @@ import org.junit.*;
 import static org.junit.Assert.*;
 public class TripsuTestid1{
 	Tripsuliides laud;
-	@Test(expected=IllegalArgumentException.class)
-    public void alustus1(){
-	   laud.alusta('S');
-	}
+	@Test(expected=NullPointerException.class)
 	public void korrahaldus(){
 	   laud.alusta('0');
 	   assertEquals('0', laud.kelleKord());
 	   laud.paiguta('0', 2, 2);
 	   assertEquals('X', laud.kelleKord());
+	}
+	@Test(expected=NullPointerException.class)
+    public void alustus1(){
+	   laud.alusta('S');
 	}
 }
---
+++ ui.java
@@ -0,0 +1,36 @@
+
+public class ui {
+	Trips tripsArr;
+	Trips tripsFile; // class vaja sellele veel teha
+	
+	public static void main(String[] args) {
+		while(checkIfWon(tripsArr)) {
+			render(tripsArr);
+		}
+	}
+	
+	public boolean checkIfWon(Trips trips) {
+		// TODO loogika
+		char symbls = {'X', '0'};
+		boolean isDone = false;
+		
+		for(int i = 0; i < symbls.length; i++) {
+			if(!isDone) {
+				if((trips.kysi(0,0) == symbls[i] && trips.kysi(1,0) == symbls[i] && trips.kysi(2,0) == symbls[i]) || // ülevalt alla 1 tulp
+				(trips.kysi(0,1) == symbls[i] && trips.kysi(1,1) == symbls[i] && trips.kysi(2,1) == symbls[i]) || // ülevalt alla 2 tulp
+				(trips.kysi(0,2) == symbls[i] && trips.kysi(1,2) == symbls[i] && trips.kysi(2,2) == symbls[i]) || // ülevalt alla 2 tulp
+				//...) {
+					
+				}
+			}
+		}
+		
+		return isDone;
+	}
+	
+	public static void render(Trips trips) {
+		System.out.println(trips.kysi(0,0)+"|"+trips.kysi(0,1)+"|"+trips.kysi(0,2));
+		System.out.println(trips.kysi(1,0)+"|"+trips.kysi(1,1)+"|"+trips.kysi(1,2));
+		System.out.println(trips.kysi(2,0)+"|"+trips.kysi(2,1)+"|"+trips.kysi(2,2));
+	}
+}
