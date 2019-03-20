8a9463b5627adcd8a16c9587980414b2d5a8f2f3
Daniel Golubkov <43164744+dangol9@users.noreply.github.com>
2019-03-20 15:45:22 +0200

    Update TripsuTestid1.java

--- TripsuTestid1.java
+++ TripsuTestid1.java
@@ -1,6 +1,7 @@
+
 import org.junit.*;
 import static org.junit.Assert.*;
-public class TripsuTestid1{
+public class TripsuTest{
 	Tripsuliides laud;
 	@Test(expected=IllegalArgumentException.class)
     public void alustus1(){
@@ -12,4 +13,30 @@ public class TripsuTestid1{
 	   laud.paiguta('0', 2, 2);
 	   assertEquals('X', laud.kelleKord());
 	}
+
+  public void CheckNum(){
+  if(kysi(x, y) == null){throw new ArithmeticException("puudub.");}
+  }
+
+  @Test(expected = ArrayOutOfIndexException.class)
+  public void CheckMore() {
+    laud.kysi(4, 2);
+  }
+
+  @Test
+  public void CheckMoreValid() {
+    assertEquals(' ', laud.kysi(1, 2));
+  }
+
+  @Test
+  public void CheckPaiguta() {
+    laud.alusta('0');
+    paiguta(1, 1);
+    assertEquals('0', laud.kysi(1, 1));
+  }
+
+  @Test(expected = ArithmeticException.class)
+  public void alustus2(){
+    laud.alusta('3');
+  }
 }
