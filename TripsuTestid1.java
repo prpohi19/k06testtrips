4371e6eb7c916a9c0a8bd7e87532e065e65f8876
Karen Minski <43170840+karenminski@users.noreply.github.com>
2019-03-21 10:19:54 +0200

    Update TripsuTestid1.java

--- TripsuTestid1.java
+++ TripsuTestid1.java
@@ -1,16 +1,30 @@
 import org.junit.*;
 import static org.junit.Assert.*;
 public class TripsuTestid1{
-	Tripsuliides laud;
-	@Test(expected=NullPointerException.class)
+	Trips laud = new Trips();
+	@Test(expected=IllegalArgumentException.class)
+    public void alustus1(){
+	   laud.alusta('S');
+    }
+    @Test
 	public void korrahaldus(){
 	   laud.alusta('0');
 	   assertEquals('0', laud.kelleKord());
 	   laud.paiguta('0', 2, 2);
 	   assertEquals('X', laud.kelleKord());
-	}
-	@Test(expected=NullPointerException.class)
-    public void alustus1(){
-	   laud.alusta('S');
+    }
+    @Test
+	public void voit(){
+	   laud.alusta('0');
+	   assertEquals('0', laud.kelleKord());
+	   laud.paiguta('0', 0, 0);
+       assertEquals('X', laud.kelleKord());
+       laud.paiguta('X', 2, 1);
+       assertEquals('0', laud.kelleKord());
+	   laud.paiguta('0', 1, 0);
+       assertEquals('X', laud.kelleKord());
+       laud.paiguta('X', 2, 2);
+       assertEquals('0', laud.kelleKord());
+	   laud.paiguta('0', 2, 0);
 	}
 }
