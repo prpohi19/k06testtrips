e83db1cd78c5f4636c5c04cadeddd4a07305eb09
Karen Minski <43170840+karenminski@users.noreply.github.com>
2019-03-27 19:48:02 +0200

    Update TripsuTestid1.java

--- TripsuTestid1.java
+++ TripsuTestid1.java
@@ -1,30 +1,67 @@
 import org.junit.*;
 import static org.junit.Assert.*;
-public class TripsuTestid1{
+public class TripsuTestid1 {
 	Trips laud = new Trips();
-	@Test(expected=IllegalArgumentException.class)
-    public void alustus1(){
-	   laud.alusta('S');
-    }
-    @Test
-	public void korrahaldus(){
-	   laud.alusta('0');
-	   assertEquals('0', laud.kelleKord());
-	   laud.paiguta('0', 2, 2);
-	   assertEquals('X', laud.kelleKord());
-    }
-    @Test
-	public void voit(){
-	   laud.alusta('0');
-	   assertEquals('0', laud.kelleKord());
-	   laud.paiguta('0', 0, 0);
-       assertEquals('X', laud.kelleKord());
-       laud.paiguta('X', 2, 1);
-       assertEquals('0', laud.kelleKord());
-	   laud.paiguta('0', 1, 0);
-       assertEquals('X', laud.kelleKord());
-       laud.paiguta('X', 2, 2);
-       assertEquals('0', laud.kelleKord());
-	   laud.paiguta('0', 2, 0);
+	@Test(expected = IllegalArgumentException.class)
+	public void alustus1() {
+		laud.alusta('S');
+	}
+	@Test
+	public void korrahaldus() {
+		laud.alusta('0');
+		assertEquals('0', laud.kelleKord());
+		laud.paiguta('0', 2, 2);
+		assertEquals('X', laud.kelleKord());
+	}
+	@Test (expected = java.lang.AssertionError.class)
+	public void veaKontroll1() {
+		laud.alusta('0');
+		assertEquals('0', laud.kelleKord());
+		laud.paiguta('0', 0, 0);
+		assertEquals('X', laud.kelleKord());
+		laud.paiguta('X', 2, 1);
+		assertEquals('0', laud.kelleKord());
+		laud.paiguta('0', 1, 0);
+		assertEquals('0', laud.kelleKord());
+		laud.paiguta('0', 2, 0);
+	}
+	@Test
+	public void veaKontroll2() {
+		laud.alusta('0');
+		assertEquals('0', laud.kelleKord());
+		laud.paiguta('0', 0, 0);
+		assertEquals('X', laud.kelleKord());
+		laud.paiguta('X', 2, 1);
+		assertEquals('0', laud.kelleKord());
+		laud.paiguta('0', 1, 0);
+		assertEquals('X', laud.kelleKord());
+		laud.paiguta('X', 2, 1);
+	}
+	@Test
+	public void veaKontroll3() {
+		laud.alusta('0');
+		assertEquals('0', laud.kelleKord());
+		laud.paiguta('0', 0, 0);
+		assertEquals('X', laud.kelleKord());
+		laud.paiguta('X', 2, 1);
+		assertEquals('0', laud.kelleKord());
+		laud.paiguta('0', 1, 0);
+		assertEquals('X', laud.kelleKord());
+		laud.paiguta('X', 0, 0);
+	}
+	@Test(expected = true)
+	public boolean voiduKontroll() {
+		laud.alusta('0');
+		assertEquals('0', laud.kelleKord());
+		laud.paiguta('0', 0, 0);
+		assertEquals('X', laud.kelleKord());
+		laud.paiguta('X', 2, 1);
+		assertEquals('0', laud.kelleKord());
+		laud.paiguta('0', 1, 0);
+		assertEquals('X', laud.kelleKord());
+		laud.paiguta('X', 2, 2);
+		assertEquals('0', laud.kelleKord());
+		laud.paiguta('0', 2, 0);
+		return tripsuObjekt.voiduKontrollimisefunktsioon()
 	}
 }
