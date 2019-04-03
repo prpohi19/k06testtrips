469ccdd68c833b89687edb65bc7eda964dd37e23
Roland Vägi <43164616+rolandvgi@users.noreply.github.com>
2019-04-03 12:34:13 +0300

    Test

--- TripsuTestid1.java
+++ TripsuTestid1.java
@@ -13,8 +13,22 @@ public class TripsuTestid1{
 	   assertEquals('X', laud.kelleKord());
 	}
 
+	@Test(expected=ArrayIndexOutOfBoundsException.class)
+	public void AlastV2ljas(){
+		laud.kysi(4, 1);
+	}
+	@Test(expected=IllegalArgumentException.class)
+    public void Numbrilinealustus(){
+	   laud.alusta('4');
+	}
+	@Test(expected=IllegalArgumentException.class) //asetamine hõivatud kohale vist?
+    public void vistonsamakohatest(){
+	   laud.alusta('X');
+		 laud.paiguta(2,2);
+		 assertEquals('X', laud.kysi(2,2));
+	}
 	//test: mängu jätkamine peale võitu
 	//test: sümboli asetamine juba hõivatud kohale
 	//test: sümboli asetamine lauast väljaspool asuvale väärtusele nt rida 4 veerg 5
-	//test: 
+	//test:
 }
